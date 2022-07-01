e2c071f3f33c0318777c47187df66f30efd5c55b
Franciel Abreu <francielabreu23@gmail.com>
2022-06-30 23:15:40 -0300

    implementação da tabela de produtos

---
+++ projeto-empresarial/database/migrations/2022_07_01_013154_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('photo');
+            $table->string('name',100);
+            $table->string('description',100);
+            $table->string('quantity',100);
+            $table->string('price',100);
+            $table->string('saleprice',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
