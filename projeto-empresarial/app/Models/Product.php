ecf77fd660a87989c9a4f1fac833d7d36ff3789e
Franciel Abreu <francielabreu23@gmail.com>
2022-07-01 19:36:47 -0300

    Implementação dos campos que o usuario poderar alterar , definição realizada na model

--- projeto-empresarial/app/Models/Product.php
+++ projeto-empresarial/app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable =['photo','name', 'description','quantity','price','saleprice'];
+
+
 }
