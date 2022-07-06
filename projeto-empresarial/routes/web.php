8888b00a60f2d326531ac04d3c7ebcd079ecec3b
Franciel Abreu <francielabreu23@gmail.com>
2022-07-06 08:22:43 -0300

    criação da rota index

--- projeto-empresarial/routes/web.php
+++ projeto-empresarial/routes/web.php
@@ -1,28 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-    /*
-    |--------------------------------------------------------------------------
-    | Web Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register web routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
-    |
-    */
 
-Route::get('/',function (){
-    return view('welcome');
-});
 
-Route::get('user/{user}',function (\App\Models\User $user){
-    return $user;
-});
-
-Route::get('/request',function (\Illuminate\Http\Request $request){
-    dd($request);
-    return 'x';
-});
-
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
