5b064ac9f0f2b2d53c9a1991bfd37ac1540d6b37
Franciel Abreu <francielabreu23@gmail.com>
2022-07-12 13:12:56 -0300

    implementação da autenticação e instalação do package breeze

--- projeto-empresarial/routes/web.php
+++ projeto-empresarial/routes/web.php
@@ -9,29 +9,31 @@ use App\Http\Controllers\{
 };
 use App\Models\Product;
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
-Route::get('/',function (){
-    return view('welcome');
+Route::get('/', function () {
+    return view('home');
 });
 
-Route::delete('/users/{id}',[UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{id}',[UserController::class, 'update'])->name('users.update');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/user', [UserController::class, 'store'])->name('users.store');
-Route::get('/users',[UserController::class, 'index'])->name('users.index');
-Route::get('/users/{id}',[UserController::class, 'show'])->name('users.show');
+
+Route::middleware(['auth'])->group(function () {
 
 
-/*--Produtos--*/
-Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
-Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/product', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/user', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
 
-
+    /*--Produtos--*/
+    Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+    Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+    Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/product', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+});
