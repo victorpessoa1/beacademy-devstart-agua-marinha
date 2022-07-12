448425315794f427c8f40030b7d792252d7febfc
Franciel Abreu <francielabreu23@gmail.com>
2022-07-12 13:11:41 -0300

    correção de erros na aplicação com o css do brezzer

--- projeto-empresarial/resources/views/auth/login.blade.php
+++ projeto-empresarial/resources/views/auth/login.blade.php
@@ -1,56 +1,29 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
+@extends('template.users')
+@section('title', 'Login')
+@section('body')
+    <h1>Login</h1>
+    <div class="justify-content-center">
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+                <label class="form-label" for="form2Example1">Email</label>
+                <input type="email" id="form2Example1" class="form-control" name="email" />
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+                <label class="form-label" for="form2Example2">Senha</label>
+                <input type="password" id="form2Example2" class="form-control" name="password"/>
+            </div>
+            <div class="col mb-5">
+                <!-- Simple link -->
+                <a href="{{ route('password.request') }}">Esqueceu a Senha?</a>
             </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-primary btn-block mb-4">Entrar</button>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+
+@endsection
--- projeto-empresarial/resources/views/auth/register.blade.php
+++ projeto-empresarial/resources/views/auth/register.blade.php
@@ -1,59 +1,34 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
+@extends('template.users')
+@section('title', 'Login')
+@section('body')
+    <h1>Registrar-Se</h1>
+    <div class="justify-content-center">
         <form method="POST" action="{{ route('register') }}">
             @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            <!-- Name input -->
+            <div class="form-outline mb-4">
+                <label class="form-label" for="name">Nome</label>
+                <input type="text" id="name" class="form-control" name="name" />
+            </div>
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+                <label class="form-label" for="email">Email</label>
+                <input type="email" id="email" class="form-control" name="email" />
             </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+                <label class="form-label" for="password">Senha</label>
+                <input type="password" id="password" class="form-control" name="password"/>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
+            <!-- Confirm Password input -->
+            <div class="form-outline mb-4">
+                <label class="form-label" for="password_confirmation">Confirme a Senha</label>
+                <input type="password" id="password_confirmation" class="form-control" name="password_confirmation" required/>
             </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-primary btn-block mb-4">Registrar</button>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+
+@endsection
