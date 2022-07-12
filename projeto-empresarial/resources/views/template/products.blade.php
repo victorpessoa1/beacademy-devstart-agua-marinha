41ca87cd83596b875b65211cf8974ec780ffe434
Franciel Abreu <francielabreu23@gmail.com>
2022-07-12 13:12:14 -0300

    implementação do cabeçalho do product

--- projeto-empresarial/resources/views/template/products.blade.php
+++ projeto-empresarial/resources/views/template/products.blade.php
@@ -9,7 +9,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<div class="container w-50 p-3">
+<div class="container w-75 p-3">
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link text-white" href="/users">Usuarios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/products">Produtos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     @yield('body')
 </div>
 </body>
